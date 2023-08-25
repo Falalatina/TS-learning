580bfe0ee6c2942d1412702936d64e7d39b20e62
Falalatina <karolina40.gorska17@gmail.com>
2023-08-25 14:22:23 +0200

    function part 1

---
+++ 02basic/myfunction.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    var w = val.toUpperCase();
+    return w;
+}
+function signUpUser(name, email, password) {
+    if (password === void 0) { password = false; }
+}
+var LoginUser = function (email, name, isPaid) {
+};
+getUpper('88m');
+addTwo(5);
+signUpUser('name', 'mops');
---
+++ 02basic/myfunction.ts
@@ -0,0 +1,23 @@
+function addTwo(num:number){
+  return num + 2;
+}
+
+function getUpper(val:string){
+  let w = val.toUpperCase();
+  return w;
+}
+
+function signUpUser(name: string, email:string, password:boolean = false){
+
+}
+
+let LoginUser = (email:string,name:string,isPaid:boolean) => {
+
+}
+
+getUpper('88m');
+addTwo(5);
+signUpUser('name','mops');
+
+
+export{}
