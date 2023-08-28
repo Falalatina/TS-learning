4a5fa8a96fb4977986bb5eeb36ca8ae6a800922d
Falalatina <karolina40.gorska17@gmail.com>
2023-08-28 14:19:33 +0200

    bad objects behaviors

---
+++ 02basic/myObjects.js
@@ -0,0 +1,9 @@
+var User = {
+    name: 'kara',
+    email: 'mops123@eu.pl',
+    isActive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+//createUser()
---
+++ 02basic/myObjects.ts
@@ -0,0 +1,16 @@
+const User ={
+  name: 'kara',
+  email: 'mops123@eu.pl',
+  isActive: true
+}
+
+function createUser({name: string, isPaid: boolean}){}
+
+
+createUser({name: 'kara', isPaid: false});
+
+ function createCourse():{name: string, price: number}{
+  return {name: 'reactjs', price: 399}
+ }
+
+export{}
