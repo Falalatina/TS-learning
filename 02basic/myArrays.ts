31e1ef42aeb975c14fc886e728f4a63df04c61fb
Falalatina <karolina40.gorska17@gmail.com>
2023-08-29 12:45:06 +0200

    Array

---
+++ 02basic/myArrays.ts
@@ -0,0 +1,20 @@
+const superHero: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string,
+  isActive: boolean
+}
+
+const allUsers: User[]= [];
+
+const MLModels: number[][] = [
+  [255,255,255],
+  [1]
+]
+
+superHero.push("spiderman");
+heroPower.push(2);
+
+allUsers.push({name: "", isActive: true});
