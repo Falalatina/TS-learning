ce5ddf4baf724cab397b3916abbb789e58c6f35b
Falalatina <karolina40.gorska17@gmail.com>
2023-08-29 12:18:09 +0200

    Type allias

--- 02basic/myObjects.ts
+++ 02basic/myObjects.ts
@@ -1,16 +1,27 @@
-const User ={
-  name: 'kara',
-  email: 'mops123@eu.pl',
-  isActive: true
+// const User ={
+//   name: 'kara',
+//   email: 'mops123@eu.pl',
+//   isActive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}){}
+
+// let newUser = {name: 'kara', isPaid: false, email: "@a.com"};
+// createUser(newUser);
+
+//  function createCourse():{name: string, price: number}{
+//   return {name: 'reactjs', price: 399}
+//  }
+
+type User ={
+  name: string,
+  email: string,
+  isActive: boolean
 }
 
-function createUser({name: string, isPaid: boolean}){}
+function createUser(user: User){}
 
+createUser({name:"wiki",email:"mops",isActive:true});
 
-createUser({name: 'kara', isPaid: false});
-
- function createCourse():{name: string, price: number}{
-  return {name: 'reactjs', price: 399}
- }
 
 export{}
