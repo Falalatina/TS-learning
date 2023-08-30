039f27942313092d24d76b3124e7e87aee420b81
Falalatina <karolina40.gorska17@gmail.com>
2023-08-30 12:31:06 +0200

    tuples problem

--- 02basic/myTupEnums.ts
+++ 02basic/myTupEnums.ts
@@ -3,4 +3,13 @@ let tUser: [string,number,boolean];
 //ustalam order!!!!!!!!!!!!!!
 tUser = ['hc', 1, true ]
 
+let rgb: [number,number,number] = [255,231,222]
+
+type User = [number,string]
+
+const newUser: User = [111, "sdds"]
+
+newUser[1] = "hc"
+newUser.push(2)
+
 export{}
