e14ed9f856d2b2898eaac9d0e72fb3556145b39f
Falalatina <karolina40.gorska17@gmail.com>
2023-08-31 13:24:40 +0200

    Update myInterface.ts

--- 02basic/myInterface.ts
+++ 02basic/myInterface.ts
@@ -5,11 +5,15 @@ interface User {
   googleId?: string
   //startTrial: () => string
   startTrial(): string //2 metoda
+  getCoupon(couponname:string, value:number):number
 }
 
 const hitesh: User = { dbId: 22, email: "hhh", userId: 2211,
 startTrial: () => {
   return "trail start";
+},
+getCoupon: (name: 'kara', val: 10) =>{
+  return 10;
 }
 }
 
