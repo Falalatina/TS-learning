f833b2fe49435589954f00248b245b8c45767b33
Falalatina <karolina40.gorska17@gmail.com>
2023-08-31 15:24:09 +0200

    Update myInterface.ts

--- 02basic/myInterface.ts
+++ 02basic/myInterface.ts
@@ -8,10 +8,19 @@ interface User {
   getCoupon(couponname:string, value:number):number
 }
 
-const hitesh: User = { dbId: 22, email: "hhh", userId: 2211,
+interface User{
+  githubToken?: string
+}
+
+interface Adminek extends User {
+  role: 'admin' | 'ta' | 'learner'
+}
+
+const hitesh: Aminek = { dbId: 22, email: "hhh", userId: 2211,
 startTrial: () => {
   return "trail start";
 },
+githubToken: 'tttfdgsyhfgh',
 getCoupon: (name: 'kara', val: 10) =>{
   return 10;
 }
