da193adb6bbd3d20a9623dd78713971378249d66
Falalatina <karolina40.gorska17@gmail.com>
2023-08-31 13:21:18 +0200

    ()=>string

--- 02basic/myInterface.ts
+++ 02basic/myInterface.ts
@@ -3,6 +3,14 @@ interface User {
   email:string,
   userId: number,
   googleId?: string
+  //startTrial: () => string
+  startTrial(): string //2 metoda
 }
 
-const hitesh: User = { dbId: 22, email: "hhh", userId: 2211}
+const hitesh: User = { dbId: 22, email: "hhh", userId: 2211,
+startTrial: () => {
+  return "trail start";
+}
+}
+
+hitesh.email = 'h@hc.com'
