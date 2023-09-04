f3a1b00dddde3148a43f786ad3a6b161f347db42
Falalatina <karolina40.gorska17@gmail.com>
2023-09-04 13:45:15 +0200

    public private

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,11 +1,20 @@
 "use strict";
+// class User {
+//   public email: string
+//   name: string
+//   readonly #city: string|number = 2; //# === private
+//   constructor(email:string, name:string){
+//     this.email =email;
+//     this.name = name;
+//   }
+// }
 class User {
-    constructor(email, name) {
-        this.city = 2;
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        this.city = 2; //# === private
     }
 }
 const hitesh = new User("mops.com", "hitesh");
-//hitesh.city = 2;
-//hitesh.city = "Jaipur"
+//hitesh.city 
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,13 +1,25 @@
+// class User {
+//   public email: string
+//   name: string
+//   readonly #city: string|number = 2; //# === private
+//   constructor(email:string, name:string){
+//     this.email =email;
+//     this.name = name;
+    
+//   }
+// }
+
 class User {
-  email: string
-  name: string
-  readonly city: string|number = 2;
-  constructor(email:string, name:string){
-    this.email =email;
-    this.name = name;
+ 
+  readonly city: string|number = 2; //# === private
+  constructor(
+    public email:string,
+    public name:string,
+    private userId?:string
+    ){
+    
+    
   }
 }
-
 const hitesh = new User( "mops.com", "hitesh");
-//hitesh.city = 2;
-//hitesh.city = "Jaipur"
+//hitesh.city 
