8e70ee9b6f693414cd81453b698672307fc2b07f
Falalatina <karolina40.gorska17@gmail.com>
2023-09-06 14:43:32 +0200

    generic

---
+++ 03purets/dist/myGeneric.js
@@ -0,0 +1,17 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+identityThree("hitesh");
+function identityFout(val) {
+    return val;
+}
+identityFout({ brand: 'olala', type: 2 });
