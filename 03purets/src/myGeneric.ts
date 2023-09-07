e93e95fc2c9172d85b2502253f72ca937db9c89c
Falalatina <karolina40.gorska17@gmail.com>
2023-09-07 19:04:10 +0200

    gereric fini

--- 03purets/src/myGeneric.ts
+++ 03purets/src/myGeneric.ts
@@ -39,9 +39,37 @@ const getMoreSearchProducts = <T,>(products: T[]): T => {
   return products[myIndex];
 }
 
-function anotherFunction<T, U>(valOne: T, valTwo: U): object {
+interface Database {
+  connection: string,
+  username: string,
+  password: string
+}
+
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
   return {
     valOne,
     valTwo
   }
+}
+
+//anotherFunction(3, {})
+
+interface Quiz{
+  name: string,
+  type: string
+}
+
+interface Course{
+  name: string,
+  author: string,
+  subject: string
+}
+
+class Sellable<T,>{
+  public cart: T[] = []
+
+  addToCart(products: T){
+    this.cart.push(products)
+  }
+
 }
