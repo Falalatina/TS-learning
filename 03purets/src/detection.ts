4ce64dfda47394004717cdab2611029e458fa8e9
Falalatina <karolina40.gorska17@gmail.com>
2023-09-08 11:47:42 +0200

    in in narrowing

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -23,3 +23,20 @@ function provideId(id: string | null){
 //     }
 //   }
 // }
+
+interface User{
+  name: string,
+  email: string
+}
+
+interface Admin{
+  name: string
+  email: string,
+  isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin){
+   if ("isAdmin" in account) {
+      return account.isAdmin
+   }
+}
