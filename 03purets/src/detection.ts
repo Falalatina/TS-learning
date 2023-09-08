8e001124511f7f068ecdf5d484937470d8a1226e
Falalatina <karolina40.gorska17@gmail.com>
2023-09-08 12:11:23 +0200

    type predict

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -40,3 +40,51 @@ function isAdminAccount(account: User | Admin){
       return account.isAdmin
    }
 }
+
+
+function logValue(x: Date | string){
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet: Fish | Bird): pet is Fish{
+  return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet:Fish | Bird) {
+  if (isFish(pet)) {
+    pet
+    return "fish food"
+  } else{
+    pet
+    return " bird food"
+  }
+}
+
+
+interface Mops{
+  snoring: () => void
+}
+interface Doberman{
+  judge: () => void
+}
+
+function isMops(dog: Mops | Doberman): dog is Mops{
+  return (dog as Mops).snoring !== undefined
+}
+
+function feedingTime(dog: Mops | Doberman){
+  if (isMops(dog)) {
+    dog
+    return "Mops is supposed to eat twice a day. reality is brutal."
+  } else{
+    dog
+    return "Doberman is supposed to eat 4 times per day"
+  }
+}
