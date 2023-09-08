d030336e1064333abdf0e0f087bb3bfa77af073c
Falalatina <karolina40.gorska17@gmail.com>
2023-09-08 14:36:41 +0200

    never

--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -88,3 +88,42 @@ function feedingTime(dog: Mops | Doberman){
     return "Doberman is supposed to eat 4 times per day"
   }
 }
+
+interface Circle{
+  kind: "circle",
+  radius: number
+}
+
+interface Square{
+  kind: "square"
+  side: number
+}
+
+interface Rectangle{
+  kind: "rectangle",
+  lenght: number,
+  width: number
+}
+
+type Shape = Circle | Square | Rectangle
+
+// function getTrueShape(shape: Shape){
+//   if (shape.kind === "circle") {
+//     return Math.PI * shape.radius **2
+//   }
+//   return shape.side * shape.side
+// }
+
+function getArea(shape: Shape){
+  switch(shape.kind){
+    case "circle":
+      return Math.PI * shape.radius **2
+    case "square":
+      return  shape.side * shape.side
+    case "rectangle":
+      return shape.lenght * shape.width
+    default:
+      const _defaultforshape: never = shape
+      return _defaultforshape
+  }
+}
